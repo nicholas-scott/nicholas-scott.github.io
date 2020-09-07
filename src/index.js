d5655886b3dfbbab6e9b3cc93e416176a87eaef6
torantella <46829523+torantella@users.noreply.github.com>
2020-09-07 13:24:52 -0400

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,5 +1,32 @@
 $(document).ready(function() {
-    $('body').click(function() {
-        console.log("Page clicked");
+    $('.nav-toggle').click(() => {
+        $('.nav-items').toggleClass('nav-items-dropped')
+    })
+    $('#button-home').click(() => {
+        $('html,body').animate({
+            scrollTop: 0
+         }, 500);
+    });
+
+    $('#button-about').click(() => {
+        $('html,body').animate({
+            scrollTop: $('.about').offset().top
+         }, 500);
+    });
+
+    $('#button-projects').click(() => {
+        $('html,body').animate({
+            scrollTop: $('.projects').offset().top
+         }, 500);
+    });
+
+    $('#button-connect').click(() => {
+        console.log("About clicked");
     });
 });
+
+// scrollTo = (id) => {
+//     $('html,body').animate({
+//        scrollTop: $("#"+id).offset().top - $(window).height()/2
+//     }, 1000);
+//  }
